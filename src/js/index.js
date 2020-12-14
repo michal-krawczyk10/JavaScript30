d2e0dcd37cb2f7c4f6d83f893a1480c51c780d88
Michał Krawczyk <michal.krawczyk10@gmail.com>
2020-12-14 23:57:19 +0100

    feat: day1 js

--- src/js/index.js
+++ src/js/index.js
@@ -9,14 +9,18 @@ const buttons = document.querySelectorAll(".day1__button--js");
 // buttons.addEventListener("click", () => {
 // 	buttons.classList.add("day1__button--click");
 // });
-for (let honk of buttons) {
-	honk.addEventListener("click", () => {
-		honk.classList.remove("day1__button--click");
-		honk.classList.add("day1__button--click");
-	});
-}
 
 window.addEventListener("keydown", function (e) {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  audio.play();
+	if (!audio) return; //stops the function
+	audio.currentTime = 0;
+	audio.play();
+	const button = document.querySelector(`li[data-key="${e.keyCode}"]`);
+	button.classList.add("day1__button--click");
 });
+
+for (let button of buttons) {
+	button.addEventListener("transitionend", () => {
+		button.classList.remove("day1__button--click");
+	});
+}
